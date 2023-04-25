5a4b75f71d3ac7449de0288b5c2645d8b218ea02
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2023-04-25 17:28:01 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+extern crate octocrab;
+extern crate tokio;
+
+use http::header::USER_AGENT;
+use octocrab::Octocrab;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::{collections::HashSet, path::Path};
+use tokio::time::Duration;
+
+pub mod repositories;
+
+// TODO: check if dot in names are ignored or not
+const KEYWORDS: &[&str] = &["setup", "author", "date", "library", "output", "title"];
+
+#[tokio::main]
+async fn main() -> octocrab::Result<()> {
+    let filename = Path::new("repos");
+    // Load or create file with unique repo names first
+    let known_repos = if Path::exists(filename) {
+        repositories::load_repos(filename).await
+    } else {
+        HashSet::<String>::new()
+    };
+    let mut new_repos = HashSet::<String>::new();
+
+    // Keywords
+    let keywords_file = Path::new("keywords.txt");
+    let keywords = repositories::load_keywords(keywords_file)
+        .await
+        .unwrap_or_else(|_| return KEYWORDS.iter().map(|s| s.to_string()).collect());
+    println!("Loaded query keywords; found {}", keywords.len());
+
+    let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
+
+    let octocrab = Octocrab::builder()
+        .personal_token(token)
+        .add_header(USER_AGENT, "programLyrique".to_string())
+        .build()?;
+
+    println!("Querying... showing only new repos");
+
+    let ctrl_c = Arc::new(AtomicBool::new(false));
+    let ctrl = ctrl_c.clone();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        // Your handler here
+        ctrl_c.store(true, Ordering::Relaxed);
+    });
+
+    for keyword in keywords {
+        println!("###### With keyword {}", keyword);
+        let mut page = 1u32;
+        let mut still_results = true;
+        while page < 1000 && still_results && !ctrl.load(Ordering::Relaxed) {
+            println!("   === Page {} ===", page);
+            let mut nb_new = 0;
+            let mut res = repositories::perform_query(&octocrab, &keyword, page).await;
+
+            let mut nb_tries = 0;
+            let mut pause = Duration::from_secs(61);
+            while let octocrab::Result::Err(octocrab::Error::GitHub {
+                source,
+                backtrace: _,
+            }) = res
+            {
+                println!("Github error: {:?}", source);
+                println!("Rate limits: {:?}", octocrab.ratelimit().get().await?);
+                if nb_tries > 10 {
+                    repositories::save_repos(filename, &new_repos).await;
+                    panic!("Already retried 10 times. Stopping. Maybe token blocked or not valid anymore?");
+                }
+                tokio::time::sleep(pause).await;
+                pause *= 2;
+                nb_tries += 1;
+                res = repositories::perform_query(&octocrab, &keyword, page).await;
+            }
+            let query = res.unwrap();
+            still_results = query.next.is_some();
+            let nb_results = query.total_count.unwrap();
+            for code in query.into_iter() {
+                let repo = code.repository.full_name.unwrap_or(code.repository.name);
+                if !known_repos.contains(&repo) && !new_repos.contains(&repo) {
+                    println!("Repository: {} ; Rmd: {}", repo, code.name);
+                    new_repos.insert(repo);
+                    nb_new += 1;
+                }
+            }
+            println!(
+                "{} new repositories on this page out of {}",
+                nb_new, nb_results
+            );
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            page += 1;
+        }
+    }
+
+    println!("Found {} new repositories in total.", new_repos.len());
+
+    // Save back the repos we have found
+    repositories::save_repos(filename, &new_repos).await;
+
+    octocrab::Result::Ok(())
+}
---
+++ src/repositories.rs
@@ -0,0 +1,68 @@
+use anyhow::Result;
+use octocrab::models::Code;
+use octocrab::{Octocrab, Page};
+use std::{collections::HashSet, path::Path};
+use tokio::fs::{File, OpenOptions};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
+
+pub async fn load_keywords(path: &Path) -> Result<Vec<String>> {
+    let file = File::open(path).await?;
+    let reader = BufReader::new(file);
+    let mut keywords = Vec::new();
+
+    let mut lines = reader.lines();
+
+    while let Some(line) = lines.next_line().await? {
+        keywords.push(line);
+    }
+    Ok(keywords)
+}
+
+pub async fn load_repos(path: &Path) -> HashSet<String> {
+    let file = File::open(path).await.unwrap();
+    let reader = BufReader::new(file);
+    let mut repos = HashSet::<String>::new();
+
+    let mut lines = reader.lines();
+
+    while let Some(line) = lines.next_line().await.unwrap() {
+        repos.insert(line);
+    }
+    repos
+}
+
+pub async fn save_repos(path: &Path, repos: &HashSet<String>) {
+    let file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .await
+        .unwrap();
+    let mut writer = BufWriter::new(file);
+
+    for repo in repos.iter() {
+        writer.write_all(repo.as_bytes()).await.unwrap();
+        writer.write_all("\n".as_bytes()).await.unwrap();
+    }
+
+    writer.flush().await.unwrap();
+}
+
+pub async fn perform_query(
+    octocrab: &Octocrab,
+    keyword: &str,
+    page: u32,
+) -> octocrab::Result<Page<Code>> {
+    // Adding keywords gives many more results
+    // In order not to bias the sampling, we can use keywords that should be present
+    // in most if not all notebooks.
+    // E.g.: output, library, title
+    // extension:Rmd or path:*.Rmd
+    octocrab
+        .search()
+        .code(&format!("{} extension:Rmd", keyword))
+        .page(page)
+        .per_page(100)
+        .send()
+        .await
+}
