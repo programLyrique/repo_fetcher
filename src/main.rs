7fb7e3325d1098d3ccb5c34d047ed49135d883df
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2023-04-26 14:30:42 +0200

    Update output

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate octocrab;
 extern crate tokio;
 
 use http::header::USER_AGENT;
+use indicatif::ProgressBar;
+use log::{error, info, warn};
 use octocrab::Octocrab;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
@@ -29,7 +31,7 @@ async fn main() -> octocrab::Result<()> {
     let keywords = repositories::load_keywords(keywords_file)
         .await
         .unwrap_or_else(|_| return KEYWORDS.iter().map(|s| s.to_string()).collect());
-    println!("Loaded query keywords; found {}", keywords.len());
+    info!("Loaded query keywords; found {}", keywords.len());
 
     let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
 
@@ -38,7 +40,7 @@ async fn main() -> octocrab::Result<()> {
         .add_header(USER_AGENT, "programLyrique".to_string())
         .build()?;
 
-    println!("Querying... showing only new repos");
+    info!("Querying... showing only new repos");
 
     let ctrl_c = Arc::new(AtomicBool::new(false));
     let ctrl = ctrl_c.clone();
@@ -49,11 +51,12 @@ async fn main() -> octocrab::Result<()> {
     });
 
     for keyword in keywords {
-        println!("###### With keyword {}", keyword);
         let mut page = 1u32;
         let mut still_results = true;
-        while page < 1000 && still_results && !ctrl.load(Ordering::Relaxed) {
-            println!("   === Page {} ===", page);
+        let mut nb_pages = None;
+        let pb = ProgressBar::new(100);
+        while page < nb_pages.unwrap_or(1000) && still_results && !ctrl.load(Ordering::Relaxed) {
+            info!("## Keyword {} -- page {}", keyword, page);
             let mut nb_new = 0;
             let mut res = repositories::perform_query(&octocrab, &keyword, page).await;
 
@@ -64,10 +67,11 @@ async fn main() -> octocrab::Result<()> {
                 backtrace: _,
             }) = res
             {
-                println!("Github error: {:?}", source);
-                println!("Rate limits: {:?}", octocrab.ratelimit().get().await?);
+                warn!("Github error: {:?}", source);
+                //println!("Rate limits: {:?}", octocrab.ratelimit().get().await?);
                 if nb_tries > 10 {
                     repositories::save_repos(filename, &new_repos).await;
+                    error!("Already retried 10 times. Stopping. Maybe token blocked or not valid anymore?");
                     panic!("Already retried 10 times. Stopping. Maybe token blocked or not valid anymore?");
                 }
                 tokio::time::sleep(pause).await;
@@ -75,27 +79,35 @@ async fn main() -> octocrab::Result<()> {
                 nb_tries += 1;
                 res = repositories::perform_query(&octocrab, &keyword, page).await;
             }
+
             let query = res.unwrap();
+
+            query.number_of_pages().map(|v| {
+                nb_pages.get_or_insert(v);
+                pb.set_length(v as u64)
+            });
             still_results = query.next.is_some();
             let nb_results = query.total_count.unwrap();
             for code in query.into_iter() {
                 let repo = code.repository.full_name.unwrap_or(code.repository.name);
                 if !known_repos.contains(&repo) && !new_repos.contains(&repo) {
-                    println!("Repository: {} ; Rmd: {}", repo, code.name);
+                    info!("Repository: {} ; Rmd: {}", repo, code.name);
                     new_repos.insert(repo);
                     nb_new += 1;
                 }
             }
-            println!(
+            info!(
                 "{} new repositories on this page out of {}",
                 nb_new, nb_results
             );
             tokio::time::sleep(Duration::from_secs(1)).await;
             page += 1;
+            pb.inc(1);
         }
+        pb.finish();
     }
 
-    println!("Found {} new repositories in total.", new_repos.len());
+    info!("Found {} new repositories in total.", new_repos.len());
 
     // Save back the repos we have found
     repositories::save_repos(filename, &new_repos).await;
