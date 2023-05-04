cec98727475ffd5c667ec00c3310ea864c93d91d
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2023-05-04 17:57:47 +0200

    Add query to fetch users's repositories for those who already have repos with notebooks

---
+++ src/users.rs
@@ -0,0 +1,5 @@
+// TODO: look at the owners of each of the repositories in the repo lists
+// and try to look at their other repositories to see if they also have Rmd files in them.
+
+// user is already stored in "repos": user/repoName
+// https://docs.github.com/en/search-github/searching-on-github/searching-code#search-within-a-users-or-organizations-repositories
